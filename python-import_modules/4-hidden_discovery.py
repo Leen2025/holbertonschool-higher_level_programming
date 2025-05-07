c0c1d5418931d3512127d87a4ab058201667d28d
Leen2025 <leeeen4@hotmail.com>
2025-05-07 07:27:33 +0000

    Add 4-hidden_discovery.py script from sandbox

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-import sys
-import os
-import importlib.util
 
+if __name__ == "__main__":
+    import importlib.util
 
-def main():
-    # the path to the .pyc file
     module_path = "/tmp/hidden_4.pyc"
 
-    # Load the module dynamically
     spec = importlib.util.spec_from_file_location("hidden_4", module_path)
     hidden_4 = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(hidden_4)
 
-    # Filter and print the names defined in the module
-    names = [name for name in dir(hidden_4) if not name.startswith('__')]
-    names.sort()
-    for name in names:
-        print(name)
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print(name)
 
-
-if __name__ == "__main__":
-    main()
