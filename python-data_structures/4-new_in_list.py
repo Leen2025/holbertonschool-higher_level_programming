002f46ac9de22aedf27b5257737eb8ef559d3747
Leen2025 <leeeen4@hotmail.com>
2025-05-12 16:55:09 +0000

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,13 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 # 4-new_in_list.py
 # Leen Alsaleh <10693@holbertonschool.com>
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
 
+def new_in_list(my_list, idx, element):
+    # Return a copy if index is invalid (negative or out of range)
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+
+    # Create a shallow copy of the original list
     copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+    copy[idx] = element  # Replace the element at the given index
+    return copy
