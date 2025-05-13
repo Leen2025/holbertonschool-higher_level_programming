163697e3c517facea87d2c96adac6578b19e52b5
Leen2025 <leeeen4@hotmail.com>
2025-05-13 12:12:26 +0000

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # 6-print_matrix_integer.py
 # Leen Alsaleh <10693@holbertonschool.com>
-
-
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
