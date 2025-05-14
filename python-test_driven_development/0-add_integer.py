be19c7dfc12187b65bde2ee1562d98774324bf2c
Leen2025 <leeeen4@hotmail.com>
2025-05-14 18:53:36 +0000

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""Write a function that adds 2 integers."""
-
-
 def add_integer(a, b=98):
-    """adds 2 integers.
+    """
+    Adds two integers or floats.
 
-    Arguments:
-        a {[int]} -- [description]
-
-    Keyword Arguments:
-        b {int} -- [description] (default: {98})
-
-    Raises:
-        TypeError: [a must be an integer]
-        TypeError: [b must be an integer]
-
-    Returns:
-        [int] -- [addition]
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+    TypeError: a must be an integer
     """
     if type(a) is float:
         a = int(a)
-
     if type(b) is float:
         b = int(b)
-
     if type(a) is not int:
         raise TypeError("a must be an integer")
-
     if type(b) is not int:
         raise TypeError("b must be an integer")
-
     return a + b
