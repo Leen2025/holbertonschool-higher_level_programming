41f99278302b4f558580040fe2485225d3862c3b
Leen2025 <leeeen4@hotmail.com>
2025-05-26 12:59:40 +0000

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-"""Defines a subclass MyList that inherits from list."""
+"""Defines a subclass MyList that inherits from list.
+
+>>> m = MyList([3, 1, 2])
+>>> m.print_sorted()
+[1, 2, 3]
+>>> print(m)
+[3, 1, 2]
+"""
 
 class MyList(list):
     """A subclass of list with a method to print sorted elements."""
