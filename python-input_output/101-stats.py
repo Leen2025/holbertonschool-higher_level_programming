1bd3ebe5419dbe451a0c78a0aa1ef54e17c5d283
Leen2025 <leeeen4@hotmail.com>
2025-06-04 10:52:21 +0000

    101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,20 +1,52 @@
 #!/usr/bin/python3
-import random
+"""Module for parsing and displaying log statistics"""
 import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write(
-        '{:d}.{:d}.{:d}.{:d} - [{}] "GET /projects/260 HTTP/1.1" {} {}\n'.format(
-            random.randint(1, 255),
-            random.randint(1, 255),
-            random.randint(1, 255),
-            random.randint(1, 255),
-            datetime.datetime.now(),
-            random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-            random.randint(1, 1024),
-        )
-    )
-    sys.stdout.flush()
+
+def print_stats(total_size, status_codes):
+    """Print the statistics"""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+def main():
+    """Main function to process input and display stats"""
+    total_size = 0
+    status_codes = {
+        200: 0, 301: 0, 400: 0, 401: 0,
+        403: 0, 404: 0, 405: 0, 500: 0
+    }
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            parts = line.split()
+
+            if len(parts) >= 7:
+                try:
+                    total_size += int(parts[-1])
+                except ValueError:
+                    pass
+
+                try:
+                    status_code = int(parts[-2])
+                    if status_code in status_codes:
+                        status_codes[status_code] += 1
+                except ValueError:
+                    pass
+
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+
+        print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
+
+
+if __name__ == "__main__":
+    main()
