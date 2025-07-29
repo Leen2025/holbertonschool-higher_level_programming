ba47cb0b1ee0f9755390c7b81dac403f62b6ce3a
Leen2025 <leeeen4@hotmail.com>
2025-07-29 19:22:46 +0000

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
-#!/user/bin/env puthon3
-def add(a:float ,b:float) ->:
-    return a+b
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
+    return a + b
